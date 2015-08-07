aee024f5ed60a362f25f6682693391bdfaf73a58
Ilya Pukhalski <ilya@pukhalski.com>
2015-08-07 14:07:13 +0200

    Init changes for TodoFlux transition

--- test-runner.sh
+++ test-runner.sh
@@ -4,7 +4,7 @@ set -o pipefail
 
 get_changes ()
 {
-	git remote add current https://github.com/tastejs/todomvc.git && \
+	git remote add current https://github.com/todoflux/todoflux.git && \
 	git fetch --quiet current && \
 	git diff HEAD origin/master --name-only |  awk 'BEGIN {FS = "/"}; {print $1 "/" $2 "/" $3}' | grep -v \/\/ | grep examples | awk -F '[/]' '{print "--framework=" $2}'|uniq
 }
@@ -18,8 +18,8 @@ then
 	cp -R ../dist/* .
 	cp ../dist/.* .
 	git add -f .
-	git config user.email 'travis@rdrei.net'
-	git config user.name 'TasteBot'
+	git config user.email 'ilya@pukhalski.com'
+	git config user.name 'pukhalski'
 	git commit -am 'update the build files for gh-pages [ci skip]'
 	# Any command that using GH_OAUTH_TOKEN must pipe the output to /dev/null to not expose your oauth token
 	git push https://${GH_OAUTH_TOKEN}@github.com/${GH_OWNER}/${GH_PROJECT_NAME} HEAD:gh-pages > /dev/null 2>&1
