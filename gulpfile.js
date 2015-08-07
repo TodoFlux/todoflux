beeae18c295880b75937606d14604b9d5d5ae243
Ilya Pukhalski <ilya@pukhalski.com>
2015-08-07 14:16:28 +0200

    Updating the rest of configuration

--- gulpfile.js
+++ gulpfile.js
@@ -97,14 +97,14 @@ gulp.task('default', ['clean'], function (cb) {
 
 // Run PageSpeed Insights
 // Update `url` below to the public URL for your site
-gulp.task('pagespeed', pagespeed.bind(null, {
+/*gulp.task('pagespeed', pagespeed.bind(null, {
 	// By default, we use the PageSpeed Insights
 	// free (no API key) tier. You can use a Google
 	// Developer API key if you have one. See
 	// http://goo.gl/RkN0vE for info key: 'YOUR_API_KEY'
 	url: 'https://todomvc.com',
 	strategy: 'mobile'
-}));
+}));*/
 
 gulp.task('serve', function (cb) {
 	app.listen(8080, cb);
